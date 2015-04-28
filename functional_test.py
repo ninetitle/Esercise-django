c3e95541ac3e5fee58f0c7edaeeade0aabe42287
ninetitle <ninetitle656@gmail.com>
2015-04-28 16:37:00 +0200

    Inserito metodo per ridurre duplicazione

--- functional_test.py
+++ functional_test.py
@@ -6,6 +6,12 @@ class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
+        
+        
+    def check_for_row_in_list_table(self,row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows= table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
     
     def tearDown(self):
         self.browser.quit()
@@ -30,25 +36,16 @@ class NewVisitorTest(unittest.TestCase):
         #he types "Buy bloodborne" in to a text box
         imputbox.send_keys('Buy Bloodborne')
 
-        #when he hit enter, the page updates, and now the page list "1:         buy Bloodborne"
+        #when he hit enter, the page updates, and now the page list "1:         buy           Bloodborne"
         imputbox.send_keys(Keys.ENTER)
-        
+        self.check_for_row_in_list_table('1. Buy Bloodborne')
+        #there still is a text box initing him to add another item
+        #he enter "Play Bloodborne on ps4"
         imputbox = self.browser.find_element_by_id('id_new_item')
         imputbox.send_keys('Play Bloodborne on ps4')
         imputbox.send_keys(Keys.ENTER)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows= table.find_elements_by_tag_name('tr')
-#        self.assertTrue(
-#            any(row.text == '1. Buy Bloodborne' for row in rows),
-#            "New to-do items do not appear on the table -- it's text  was:\n%s" %(table.text,)
-#        )
-        self.assertIn('1. Buy Bloodborne', [row.text for row in rows])
-        self.assertIn('2. Play Bloodborne on ps4', [row.text for row in rows])
-        #there still is a text box initing him to add another item
-        #he enter "Play Bloodborne on ps4"
-
         #the page update again, and now there are both item on the list
-
+        self.check_for_row_in_list_table('2. Play Bloodborne on ps4')
         #Marci wonder if the site will remember the list for him
         #Then he sees that the site had generated an unique url for             him--- there 
         self.fail('finish the test !')
