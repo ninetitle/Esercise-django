a6a6136926c01ce837dd70a688b426075236b5c0
ninetitle <ninetitle656@gmail.com>
2015-05-02 19:10:28 +0200

    First step for layout and styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -16,7 +16,19 @@ class NewVisitorTest(LiveServerTestCase):
     
     def tearDown(self):
         self.browser.quit()
+    
+    def test_layout_and_styling(self):
+        #Marci go to the homepage
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
         
+        #and notice that the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta = 5        
+        )
     def test_can_start_list_and_retrieve_later(self):
         
         #Marci had heard of a cool new online to-do app.
