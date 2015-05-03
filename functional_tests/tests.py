ca123eafc9375383cbb3e27968e2b74bebb1fc90
ninetitle <ninetitle656@gmail.com>
2015-05-03 17:39:25 +0200

    Now we use bootstrap for layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,34 +1,37 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
-        
+    
+    def tearDown(self):
+        self.browser.refresh()
+        self.browser.quit()       
         
     def check_for_row_in_list_table(self,row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows= table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
     
-    def tearDown(self):
-        self.browser.quit()
-    
     def test_layout_and_styling(self):
         #Marci go to the homepage
         self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024, 768)
-        
+           
         #and notice that the input box is nicely centered
+            
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
+            505,
             delta = 5        
         )
+    
+
     def test_can_start_list_and_retrieve_later(self):
         
         #Marci had heard of a cool new online to-do app.
@@ -41,27 +44,28 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('To-Do',header_text)
         
         #He is invited to enter a to-do list straight away
-        imputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-            imputbox.get_attribute('placeholder'),
+            inputbox.get_attribute('placeholder'),
             'enter a To-Do item'
         )
         #he types "Buy bloodborne" in to a text box
-        imputbox.send_keys('Buy Bloodborne')
+        inputbox.send_keys('Buy Bloodborne')
 
         #when he hit enter, he is taken to another page with the list "1:buy Bloodborne"
-        imputbox.send_keys(Keys.ENTER)
+        inputbox.send_keys(Keys.ENTER)
         Marci_list_url = self.browser.current_url
         self.assertRegexpMatches(Marci_list_url,'/lists/.+')
         self.check_for_row_in_list_table('1. Buy Bloodborne')
-        
+
         #there still is a text box initing him to add another item
         #he enter "Play Bloodborne on ps4"
-        imputbox = self.browser.find_element_by_id('id_new_item')
-        imputbox.send_keys('Play Bloodborne on ps4')
-        imputbox.send_keys(Keys.ENTER)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Play Bloodborne on ps4')
+        inputbox.send_keys(Keys.ENTER)
         #the page update again, and now there are both item on the list
         self.check_for_row_in_list_table('2. Play Bloodborne on ps4')
+        self.check_for_row_in_list_table('1. Buy Bloodborne')
         
         #Now another users, Francesco, come along to the site.
         #We use a new browser session to ensure that no information#
@@ -100,5 +104,6 @@ class NewVisitorTest(LiveServerTestCase):
         #he visit that url, and the list is still there
 
         #satisfied, he goes to sleep
+    
 
 
--- superlist/settings.py
+++ superlist/settings.py
@@ -50,6 +50,22 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'superlist.urls'
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 WSGI_APPLICATION = 'superlist.wsgi.application'
 
 
