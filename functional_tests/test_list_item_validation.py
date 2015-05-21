0a23d13a0d7f2908186d4659bd2d15436a6c93cc
ninetitle <ninetitle656@gmail.com>
2015-05-21 16:57:05 +0200

    Added new functional test for hiding the item error on typing

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,5 +1,8 @@
 from .base import functionalTest
-class itemValidationTest(functionalTest):  
+class itemValidationTest(functionalTest):
+    
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
     
     def test_cannot_add_empty_list_items(self):
         #Marci go to the home page and accidently try to submit an empty item
@@ -8,7 +11,7 @@ class itemValidationTest(functionalTest):
         self.inserisci_textbox('id_text','\n')
         #The home page refresh and now these is an error message saying
         #that the list item cannot be blank
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text,"You can't have an empty list item")
         
         #He tries with a real item that work
@@ -19,7 +22,7 @@ class itemValidationTest(functionalTest):
         
         self.browser.find_element_by_id('id_text').send_keys('\n')
         self.check_for_row_in_list_table('1. Buy milk')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text,"You can't have an empty list item")
         
         #and he can correct it by filling some text in
@@ -38,5 +41,19 @@ class itemValidationTest(functionalTest):
         
         #He sees an helpful error message
         self.check_for_row_in_list_table('1. Buy the witcher 3')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, "You have already got this in your list")
+        
+    def test_error_messages_are_cleared_on_imput(self):
+        #Marci start a new list in a way that cause validetion error
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('\n')
+        error = self.get_error_element()
+        self.assertTrue(error.is_displayed())
+        
+        #He begin to type to clear the error
+        self.get_item_input_box().send_keys('B')
+        
+        #He is pleased to see that the error message is gone
+        error = self.get_error_element()
+        self.assertFalse(error.is_displayed())
